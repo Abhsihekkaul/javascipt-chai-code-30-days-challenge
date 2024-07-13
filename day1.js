3396600b7aa20dd96da3ae7bc0cebc62a9a48585
Abhishek kaul <Abhishekkaul32@gmail.com>
2024-07-13 08:07:46 +0530

    DAY 1 DONE

---
+++ day1.js
@@ -0,0 +1,28 @@
+// Activity 1 
+var a = 10
+console.log(a)
+
+let myName = "Abhishek kaul"
+console.log(myName)
+
+// Activity 2
+const valueOfPie = 3.141592653
+console.log(valueOfPie)
+
+// activity 3
+console.log(typeof(a))
+console.log(typeof(myName))
+console.log(typeof(valueOfPie))
+
+// activity 4 
+let changeVariable = 10
+console.log(changeVariable)
+changeVariable = 12
+console.log(changeVariable)
+
+// activity 5
+const checkingContantChange = 10
+console.log(checkingContantChange)
+
+// checkingContantChange = 12
+// console.log(checkingContantChange)
