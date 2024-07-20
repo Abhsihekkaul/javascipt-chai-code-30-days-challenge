c53977d73e4500ac678a3c0659f4c19b4be3c310
Abhishek kaul <Abhishekkaul32@gmail.com>
2024-07-20 09:16:06 +0530

    About the completion of day 6

---
+++ day6.js
@@ -0,0 +1,50 @@
+// task 1
+
+const arr1 = [1,2,3,4,5];
+console.log(arr1);
+
+// task 2
+
+console.log(arr1[0]);
+
+// task 3
+
+arr1.push(6);
+arr1.push(7);
+
+console.log(arr1);
+
+// task 4
+arr1.pop();
+console.log(arr1);
+
+// task 5
+arr1.shift();
+console.log(arr1);
+
+// task 6
+arr1.unshift(0);
+console.log(arr1);
+
+// task 7
+
+// task 8
+
+// task 9
+
+// task 10
+
+for(let i = 0; i < arr1.length; i++){
+    console.log(arr1[i]);
+}
+console.log('\n')
+// task 11
+arr1.forEach(element => {
+    console.log(element);
+});
+
+console.log("\n")
+// task 12
+
+arr2 = [[1,2],[2,1]]
+console.log(arr2[0][1])
